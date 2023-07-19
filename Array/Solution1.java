d6f2837da45ff04c0836dbd94b5082224e3df70c
gauravmaurya <gauravmaurya3825@gmail.com>
2023-07-19 15:29:37 +0530

    first commit

---
+++ Array/Solution1.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution1 {
+  public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            Integer requiredNum = target - numbers[i];
+            if (indexMap.containsKey(requiredNum)) {
+                return new int[] {indexMap.get(requiredNum), i};
+            }
+            indexMap.put(numbers[i], i);
+        }
+        return new int[] {-1, -1};
+    }
+}   
---
+++ Array/Solution2.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Solution2 {
+    public void moveZeroes(int[] nums) {
+        int firstZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(firstZero, i, nums);
+                firstZero++;
+            }
+        }
+    }
+
+    private void swap(int index1, int index2, int[] numbers) {
+        int val2 = numbers[index2];
+        numbers[index2] = numbers[index1];
+        numbers[index1] = val2;
+    }
+}
